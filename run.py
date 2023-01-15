0f2afbc996c69ac3c9ba4986987ae683c391a885
Ido Koubi <66007024+IuLax790@users.noreply.github.com>
2023-01-15 22:00:47 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,13 +2,20 @@ from booking.booking import Booking
 try:
     with Booking() as bot:
         bot.land_first_page()
-        bot.change_currency(currency='GBP')
-        bot.select_place_to_go('New York')
-        bot.select_dates(check_in_date="2023-05-19",
-                         check_out_date="2023-05-28")
-        bot.select_adults(1)
+        bot.change_currency(currency='USD')
+        bot.select_place_to_go(
+            input("Where you want to go ?"))
+        bot.select_dates(check_in_date=input(
+            "What is the check in date?"
+        ),
+                         check_out_date=input(
+            "What is the check out date?"))
+        bot.select_adults(int(input("How many people ?")))
         bot.click_search()
         bot.apply_filtrations()
+        bot.refresh() # a workaround to let our bot
+                      # to grab the data properly
+        bot.report_results()
 except Exception as e:
     if 'in PATH' in str(e):
         print('Your are trying to run the bot'
